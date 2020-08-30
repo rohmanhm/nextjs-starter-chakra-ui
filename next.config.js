d0e0b59cd7dbe983e6fbe1b1f3759d5412047448
Rohman Habib M <mhrohman@live.com>
2020-08-30 23:29:16 +0700

    feat(config): add next-pwa (#3)

--- next.config.js
+++ next.config.js
@@ -2,12 +2,20 @@
 
 const withPlugins = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')
+const withPWA = require('next-pwa')
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const plugins = [bundleAnalyzer]
+const pwa = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+  },
+})
+
+const plugins = [bundleAnalyzer, pwa]
 
 module.exports = withPlugins(plugins, {
   poweredByHeader: false,
