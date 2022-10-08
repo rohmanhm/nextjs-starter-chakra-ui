0591386ef4cf6b249ea31ca6d0fb1cd12f5c3115
Rohman H Muhammad <mhrohman@live.com>
2022-10-08 13:11:14 +0700

    chore: upgrade dependencies per Q4/2022

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
-import theme from '@/theme'
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
-import React from 'react'
+
+import theme from '@/theme'
 
 export default class Document extends NextDocument {
   render() {
